7b9aabbf902ee43a85c564099008b732982bdcdb
Nikola Zeravcic <support@bytenet.rs>
2021-07-19 23:28:19 +0200

    build(spam-protect): add Protector Widget prerequisites for necessary verification

--- src/SpamProtect/ProtectorWidgetImpl.php
+++ src/SpamProtect/ProtectorWidgetImpl.php
@@ -1,9 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ZexBre\SpamProtect;
 
+use ZexBre\SpamProtect\Contracts\VerificatorWidget;
 use ZexBre\SpamProtect\Contracts\ProtectorWidget;
+use ZexBre\SpamProtect\DataStructures\WidgetResponse;
 
 class ProtectorWidgetImpl implements ProtectorWidget
 {
+    public function render(VerificatorWidget $verificatorWidget): WidgetResponse
+    {
+        return $verificatorWidget->render();
+    }
 }
